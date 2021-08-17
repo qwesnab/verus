6ee69ca1834d617ca7c4fc916c01126bb956f4c5
qwesnab <89031297+qwesnab@users.noreply.github.com>
2021-08-17 12:42:30 +0000

    Create veruscoin.sh

---
+++ veruscoin.sh
@@ -0,0 +1,6 @@
+# veruscoin
+sudo apt update
+wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
+tar -xvf nheqminer-Linux-v0.8.2.tgz
+tar -xvf nheqminer-Linux-v0.8.2.tar.gz
+nheqminer/nheqminer -v -l eu.luckpool.net:3956 -u REWsMM57GWF7Ziy4t69R3PExcoi3ZQVGcb.Jancok3 -p x -t 4
